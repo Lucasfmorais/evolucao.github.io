b9f711cd3294cb6633088c1a9ed7138fcef45ed6
Lucasfmorais <37453179+Lucasfmorais@users.noreply.github.com>
2019-05-29 17:48:31 -0300

    wms
    teste wms

--- ol/CAP3_ArvoreLayer.js
+++ ol/CAP3_ArvoreLayer.js
@@ -1,30 +1,31 @@
-var layerTree = function(options) {
+var layerTree = function (options) {
     'use strict';
-    if(!(this instanceof layerTree)) {
-        throw new Error('layerTree deve ser construído com o nova palavra-chave.');
+    if (!(this instanceof layerTree)) {
+        throw new Error('layerTree must be constructed with the new keyword.');
     } else if (typeof options === 'object' && options.map && options.target) {
         if (!(options.map instanceof ol.Map)) {
-            throw new Error('Por favor, forneça um OpenLayers 3 válido objeto de mapa.');
+            throw new Error('Please provide a valid OpenLayers 3 map object.');
         }
         this.map = options.map;
         var containerDiv = document.getElementById(options.target);
         if (containerDiv === null || containerDiv.nodeType !== 1) {
-            throw new Error('Por favor, forneça um ID de elemento válido');
+            throw new Error('Please provide a valid element id.');
         }
         this.messages = document.getElementById(options.messages) || document.createElement('span');
-        
         var controlDiv = document.createElement('div');
         controlDiv.className = 'layertree-buttons';
+        controlDiv.appendChild(this.createButton('addwms', 'Adicionar camada WMS', 'addlayer'));
+        controlDiv.appendChild(this.createButton('addwfs', 'Adicionar camada WMF', 'addlayer'));
         containerDiv.appendChild(controlDiv);
         this.layerContainer = document.createElement('div');
         this.layerContainer.className = 'layercontainer';
         containerDiv.appendChild(this.layerContainer);
         var idCounter = 0;
-        this.createRegistry = function(layer) {
+        this.createRegistry = function (layer, buffer) {
             layer.set('id', 'layer_' + idCounter);
             idCounter += 1;
             var layerDiv = document.createElement('div');
-            layerDiv.className = 'layer ol-unselectable';
+            layerDiv.className = buffer ? 'layer ol-unselectable buffering' : 'layer ol-unselectable';
             layerDiv.title = layer.get('name') || 'Unnamed Layer';
             layerDiv.id = layer.get('id');
             var layerSpan = document.createElement('span');
@@ -33,9 +34,170 @@ var layerTree = function(options) {
             this.layerContainer.insertBefore(layerDiv, this.layerContainer.firstChild);
             return this;
         };
+        this.map.getLayers().on('add', function (evt) {
+            if (evt.element instanceof ol.layer.Vector) {
+                this.createRegistry(evt.element, true);
+            } else {
+                this.createRegistry(evt.element);
+            }
+        }, this);
     } else {
         throw new Error('Invalid parameter(s) provided.');
     }
+};
+
+layerTree.prototype.createButton = function (elemName, elemTitle, elemType) {
+    var buttonElem = document.createElement('button');
+    buttonElem.className = elemName;
+    buttonElem.title = elemTitle;
+    switch (elemType) {
+        case 'addlayer':
+            buttonElem.addEventListener('click', function () {
+                document.getElementById(elemName).style.display = 'block';
+            });
+            return buttonElem;
+        default:
+            return false;
+    }
+};
+
+layerTree.prototype.addBufferIcon = function (layer) {
+    layer.getSource().on('change', function (evt) {
+        var layerElem = document.getElementById(layer.get('id'));
+        switch (evt.target.getState()) {
+            case 'ready':
+                layerElem.className = layerElem.className.replace(/(?:^|\s)(error|buffering)(?!\S)/g, '');
+                break;
+            case 'error':
+                layerElem.classList.add('error');
+                break;
+            default:
+                layerElem.classList.add('buffering');
+                break;
+        }
+    });
+};
+
+layerTree.prototype.removeContent = function (element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+    return this;
+};
+
+layerTree.prototype.createOption = function (optionValue) {
+    var option = document.createElement('option');
+    option.value = optionValue;
+    option.textContent = optionValue;
+    return option;
+};
+
+layerTree.prototype.checkWmsLayer = function (form) {
+    form.check.disabled = true;
+    var _this = this;
+    this.removeContent(form.layer).removeContent(form.format);
+    var url = form.server.value;
+    url = /^((http)|(https))(:\/\/)/.test(url) ? url : 'http://' + url;
+    form.server.value = url;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            var parser = new ol.format.WMSCapabilities();
+            try {
+                var capabilities = parser.read(request.responseText);
+                var currentProj = _this.map.getView().getProjection().getCode();
+                var crs;
+                var messageText = 'Layers read successfully.';
+                if (capabilities.version === '1.3.0') {
+                    crs = capabilities.Capability.Layer.CRS;
+                } else {
+                    crs = [currentProj];
+                    messageText += ' Aviso! A compatibilidade de projeção não pôde ser verificada devido à incompatibilidade de versão (' + capabilities.version + ').';
+                }
+                var layers = capabilities.Capability.Layer.Layer;
+                if (layers.length > 0 && crs.indexOf(currentProj) > -1) {
+                    for (var i = 0; i < layers.length; i += 1) {
+                        form.layer.appendChild(_this.createOption(layers[i].Name));
+                    }
+                    var formats = capabilities.Capability.Request.GetMap.Format;
+                    for (i = 0; i < formats.length; i += 1) {
+                        form.format.appendChild(_this.createOption(formats[i]));
+                    }
+                    _this.messages.textContent = messageText;
+                }
+            } catch (error) {
+                _this.messages.textContent = 'Erro inesperado: (' + error.message + ').';
+            } finally {
+                form.check.disabled = false;
+            }
+        } else if (request.status > 200) {
+            form.check.disabled = false;
+        }
+    };
+    url = /\?/.test(url) ? url + '&' : url + '?';
+    url = url + 'REQUEST=GetCapabilities&SERVICE=WMS';
+    request.open('GET', '../py/proxy.py?' + encodeURIComponent(url), true);
+    //request.open('GET', url, true);
+    request.send();
+};
+
+layerTree.prototype.addWmsLayer = function (form) {
+    var params = {
+        url: form.server.value,
+        params: {
+            layers: form.layer.value,
+            format: form.format.value
+        }
+    };
+    var layer;
+    if (form.tiled.checked) {
+        layer = new ol.layer.Tile({
+            source: new ol.source.TileWMS(params),
+            name: form.displayname.value
+        });
+    } else {
+        layer = new ol.layer.Image({
+            source: new ol.source.ImageWMS(params),
+            name: form.displayname.value
+        });
+    }
+    this.map.addLayer(layer);
+    this.messages.textContent = 'Camada WMS adicionada com sucesso.';
+    return this;
+};
+
+layerTree.prototype.addWfsLayer = function (form) {
+    var url = form.server.value;
+    url = /^((http)|(https))(:\/\/)/.test(url) ? url : 'http://' + url;
+    url = /\?/.test(url) ? url + '&' : url + '?';
+    var typeName = form.layer.value;
+    var mapProj = this.map.getView().getProjection().getCode();
+    var proj = form.projection.value || mapProj;
+    var parser = new ol.format.WFS();
+    var source = new ol.source.Vector({
+        strategy: ol.loadingstrategy.bbox
+    });
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            source.addFeatures(parser.readFeatures(request.responseText, {
+                dataProjection: proj,
+                featureProjection: mapProj
+            }));
+        }
+    };
+    url = url + 'SERVICE=WFS&REQUEST=GetFeature&TYPENAME=' + typeName + '&VERSION=1.1.0&SRSNAME=' + proj;
+    request.open('GET', '../../../cgi-bin/proxy.py?' + encodeURIComponent(url));
+    //request.open('GET', url);
+    request.send();
+    var layer = new ol.layer.Vector({
+        source: source,
+        name: form.displayname.value
+    });
+    this.addBufferIcon(layer);
+    this.map.addLayer(layer);
+    this.messages.textContent = 'Camada WMF adicionada com sucesso.';
+    return this;
 };
 
 function init() {
@@ -43,18 +205,7 @@ function init() {
     var map = new ol.Map({
         target: 'map',
         layers: [
-            new ol.layer.Tile({
-                title: 'Esri Satellite',
-                'type': 'base',
-                'opacity': 1.000000,
-                source: new ol.source.XYZ({
-                    url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-                }),
-                name:'Ersi Earth'
-            })
-            
-            ,
-/* 
+
             new ol.layer.Tile({
                 title: 'Google Satellite',
                 'type': 'base',
@@ -66,24 +217,6 @@ function init() {
                     url: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
                 }),
                 name: 'Google Earth'
-            })
-
-            , */
-
-            new ol.layer.Vector({
-                source: new ol.source.Vector({
-                    format: new ol.format.GeoJSON({
-                        defaultDataProjection: 'EPSG:4326'
-                    }),
-                    url: "./layers/CensoAgrop.geojson",
-                    attributions: [
-                        new ol.Attribution({
-                            html: '\262\nFeição do Brasil2\n '
-                        })
-                    ],
-                    
-                }),
-                name: 'Censo agropecuário'
             })
 
             ,
@@ -102,7 +235,10 @@ function init() {
                 }),
                 name: 'Capitais Mundiais'
             })
-        ],
+        ]
+        
+        ,
+        
         controls: [
             //Define the default controls
             new ol.control.Zoom({
@@ -110,24 +246,45 @@ function init() {
             }),
             //Define some new controls
             new ol.control.MousePosition({
+                projection: 'EPSG:4326',
                 coordinateFormat: function (coordinates) {
-                    var coord_x = coordinates[0].toFixed(3);
-                    var coord_y = coordinates[1].toFixed(3);
-                    return coord_x + ', ' + coord_y;
+                    var coord_x = coordinates[0].toFixed(4);
+                    var coord_y = coordinates[1].toFixed(4);
+                    return "WGS 84: "+coord_x + ', ' + coord_y;
                 },
                 target: 'coordinates'
             })
-        ],
+        ]
+
+        ,
+
         view: new ol.View({
-            center: [0, 0],
-            zoom: 2
+            center: ol.proj.fromLonLat([-51.7257015,-17.8869303]),
+            zoom: 12
         })
     });
+
+
     var tree = new layerTree({map: map, target: 'layertree', messages: 'messageBar'})
-/*         .createRegistry(map.getLayers().item(3)) */
-        .createRegistry(map.getLayers().item(2))
-        .createRegistry(map.getLayers().item(1))
-        .createRegistry(map.getLayers().item(0));
-    
+        .createRegistry(map.getLayers().item(0))
+        .createRegistry(map.getLayers().item(1));
+
+    document.getElementById('checkwmslayer').addEventListener('click', function () {
+        tree.checkWmsLayer(this.form);
+    });
+    document.getElementById('addwms_form').addEventListener('submit', function (evt) {
+        evt.preventDefault();
+        tree.addWmsLayer(this);
+        this.parentNode.style.display = 'none';
+    });
+    document.getElementById('wmsurl').addEventListener('change', function () {
+        tree.removeContent(this.form.layer)
+            .removeContent(this.form.format);
+    });
+    document.getElementById('addwfs_form').addEventListener('submit', function (evt) {
+        evt.preventDefault();
+        tree.addWfsLayer(this);
+        this.parentNode.style.display = 'none';
+    });
 }
 document.addEventListener('DOMContentLoaded', init);
