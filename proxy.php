194cc06787234e1722d2d498baa647fda5d15e29
Lucasfmorais <37453179+Lucasfmorais@users.noreply.github.com>
2019-06-27 11:07:22 -0300

    s

--- proxy.php
+++ proxy.php
@@ -3,7 +3,7 @@
 header('content-type: application/xml');
 //http://api.worldbank.org/countries?per_page=10&incomeLevel=LIC
 $c= $_GET['a'];
-    if($c=="pjm"){
+    if($c=="pjam"){
         $url = 'http://api.worldbank.org/v2/country/all/indicator/SP.POP.TOTL?date=2015:2015&page=4';
     }else{
         $url = 'http://api.worldbank.org/v2/country/all/indicator/SP.POP.TOTL?date=2015:2015&page=4';
